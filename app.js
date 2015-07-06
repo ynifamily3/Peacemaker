b4da7a3909065476ed4db41d294af439f8da25a8
Seungho Kim <niceb5y@gmail.com>
2015-07-06 15:41:42 +0900

    implement session login without db

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var user = require('./routes/user');
 
 var app = express();
 
@@ -21,9 +22,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: '98bec3f956899257d0f02ae3810aa0f2',
+  resave: false,
+  saveUninitialized: true
+}));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/user', user);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/user.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  if (!req.session.username) {
+  	res.redirect('/user/login');
+  } else {
+  	res.redirect('/user/hello')
+  }
+});
+
+router.get('/login', function(req, res, next) {
+	res.render('user_login');
+});
+
+router.post('/login', function(req, res, next) {
+	req.session.username = req.body.username;
+	res.redirect('/user');
+});
+
+router.get('/hello', function(req, res, next) {
+	res.render('user_hello', {name:req.session.username});
+});
+
+module.exports = router;
