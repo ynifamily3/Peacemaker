823929ee024bc408e2b95b7b362090c531e602b6
Seungho Kim <niceb5y@gmail.com>
2015-07-26 21:21:38 +0900

    add user page

--- routes/user.js
+++ routes/user.js
@@ -23,7 +23,13 @@ var parseForm = bodyParser.urlencoded({extended: false});
 router.get('/:user', function(req, res, next) {
 	connection.query('select * from users where username = ?', [req.params.user], function(err, result) {
 		if (err) throw err;
-		res.json(result[0]);
+		var date = new Date(result[0].register_date);
+		var dateString = date.getFullYear() + '년 ' + (date.getMonth() + 1) + '월';
+		res.render('user_user', {
+			name: req.session.name,
+			user_name: result[0].name,
+			register_date: dateString
+		});
 	});
 });
 
