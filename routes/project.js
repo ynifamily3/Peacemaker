1e64399c948c45744022db584d8df875d6387543
Seungho Kim <niceb5y@gmail.com>
2015-08-10 19:04:18 +0900

    add project join page

--- routes/project.js
+++ routes/project.js
@@ -26,7 +26,41 @@ router.get('/:project', function(req, res, next) {
 	} else {
 		connection.query('select * from projects where url = ?', [req.params.project], function(err, result) {
 			if (err) throw err;
-			res.render('project_index', {
+			var _project = result[0];
+			connection.query('select * from project_entries where pid = ? and id = ?', [req.session.pid, _project.id], function(err, result) {
+				if (result.length == 0) {
+					res.redirect('/p/' + req.params.project + '/join');
+				} else {
+					res.render('project_index', {
+						user: {
+							name: req.session.name,
+							username: req.session.username,
+							pid: req.session.pid
+						},
+						project: {
+							id: _project.id,
+							url: _project.url,
+							name: _project.name,
+							desc: _project.desc,
+							admin_id: _project.admin_id
+						},
+						title: _project.name
+					});
+				};
+				if (err) throw err;
+			});
+		});
+	};
+});
+
+router.get('/:project/join', function(req, res, next) {
+	if (!req.session.name) {
+		res.redirect('/user/login');
+	} else {
+		connection.query('select * from projects where url = ?', [req.params.project], function(err, result) {
+			date = new Date(result[0].created_date);
+			if (err) throw err;
+			res.render('project_join', {
 				user: {
 					name: req.session.name,
 					username: req.session.username,
@@ -38,7 +72,8 @@ router.get('/:project', function(req, res, next) {
 					name: result[0].name,
 					desc: result[0].desc,
 					admin_id: result[0].admin_id,
-					created_date: result[0].created_date
+					created_date: date.getFullYear() + '년 ' + (date.getMonth() + 1) + '월'
+
 				},
 				title: result[0].name
 			});
