d037ca235eaad3af7f321ba0fbee5e2bc801290a
Seungho Kim <niceb5y@gmail.com>
2015-08-11 22:17:41 +0900

    improve calendar

--- public/js/project_calendar.js
+++ public/js/project_calendar.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
   $('#calendar').fullCalendar({
-   lang: 'ko'
-	})
+  	header: {
+    	left: 'title',
+    	center: '',
+    	right: 'month,agendaDay today prev,next'
+		},
+  	lang: 'ko',
+		dayClick: function(date, jsEvent, view) {
+		$('#calendar').fullCalendar('changeView', 'agendaDay');
+		$('#calendar').fullCalendar('gotoDate', date);
+   }
+	});
 });
