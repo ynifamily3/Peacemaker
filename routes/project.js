5d1e3365eae39fa028b67c02c9e86bca2d862ce2
Seungho Kim <niceb5y@gmail.com>
2015-08-12 21:27:44 +0900

    simplify queries

--- routes/project.js
+++ routes/project.js
@@ -27,32 +27,28 @@ router.get('/:project', function(req, res, next) {
 	if (!req.session.name) {
 		res.redirect('/user/login');
 	} else {
-		connection.query('select * from projects where url = ?', [req.params.project], function(err, result) {
+		connection.query('select * from project_entries join projects on projects.id = project_entries.id where pid = ? and url = ?', [req.session.pid, req.params.project], function(err, result) {
 			if (err) throw err;
-			var _project = result[0];
-			connection.query('select * from project_entries where pid = ? and id = ?', [req.session.pid, _project.id], function(err, result) {
-				if (err) throw err;
-				if (result.length == 0) {
-					res.redirect('/p/' + req.params.project + '/join');
-				} else {
-					res.render('project_index', {
-						user: {
-							name: req.session.name,
-							username: req.session.username,
-							pid: req.session.pid
-						},
-						project: {
-							id: _project.id,
-							url: _project.url,
-							name: _project.name,
-							desc: _project.desc,
-							admin_id: _project.admin_id,
-							hangout_url: _project.hangout_url
-						},
-						title: _project.name
-					});
-				};
-			});
+			if (result.length == 0) {
+				res.redirect('/p/' + req.params.project + '/join');
+			} else {
+				res.render('project_index', {
+					user: {
+						name: req.session.name,
+						username: req.session.username,
+						pid: req.session.pid
+					},
+					project: {
+						id: result[0].id,
+						url: result[0].url,
+						name: result[0].name,
+						desc: result[0].desc,
+						admin_id: result[0].admin_id,
+						hangout_url: result[0].hangout_url
+					},
+					title: result[0].name
+				});
+			};
 		});
 	};
 });
@@ -61,40 +57,36 @@ router.get('/:project/calendar', function(req, res, next) {
 	if (!req.session.name) {
 		res.redirect('/user/login');
 	} else {
-		connection.query('select * from projects where url = ?', [req.params.project], function(err, result) {
+		connection.query('select * from project_entries join projects on projects.id = project_entries.id where pid = ? and url = ?', [req.session.pid, req.params.project], function(err, result) {
 			if (err) throw err;
-			var _project = result[0];
-			connection.query('select * from project_entries where pid = ? and id = ?', [req.session.pid, _project.id], function(err, result) {
-				if (err) throw err;
-				if (result.length == 0) {
-					res.redirect('/p/' + req.params.project + '/join');
-				} else {
-					res.render('project_calendar', {
-						user: {
-							name: req.session.name,
-							username: req.session.username,
-							pid: req.session.pid
-						},
-						project: {
-							id: _project.id,
-							url: _project.url,
-							name: _project.name,
-							desc: _project.desc,
-							admin_id: _project.admin_id,
-							hangout_url: _project.hangout_url
-						},
-						title: _project.name,
-						js: [
-							'moment.js',
-							'fullcalendar.js',
-							'fullcalendar-ko.js'
-						],
-						css: [
-							'fullcalendar.css'
-						]
-					});
-				};
-			});
+			if (result.length == 0) {
+				res.redirect('/p/' + req.params.project + '/join');
+			} else {
+				res.render('project_calendar', {
+					user: {
+						name: req.session.name,
+						username: req.session.username,
+						pid: req.session.pid
+					},
+					project: {
+						id: result[0].id,
+						url: result[0].url,
+						name: result[0].name,
+						desc: result[0].desc,
+						admin_id: result[0].admin_id,
+						hangout_url: result[0].hangout_url
+					},
+					title: result[0].name,
+					js: [
+						'moment.js',
+						'fullcalendar.js',
+						'fullcalendar-ko.js'
+					],
+					css: [
+						'fullcalendar.css'
+					]
+				});
+			};
 		});
 	};
 });
@@ -103,37 +95,33 @@ router.get('/:project/hangout', csrfProtection, function(req, res, next) {
 	if (!req.session.name) {
 		res.redirect('/user/login');
 	} else {
-		connection.query('select * from projects where url = ?', [req.params.project], function(err, result) {
+		connection.query('select * from project_entries join projects on projects.id = project_entries.id where pid = ? and url = ?', [req.session.pid, req.params.project], function(err, result) {
 			if (err) throw err;
-			var _project = result[0];
-			connection.query('select * from project_entries where pid = ? and id = ?', [req.session.pid, _project.id], function(err, result) {
-				if (err) throw err;
-				if (result.length == 0) {
-					res.redirect('/p/' + req.params.project + '/join');
+			if (result.length == 0) {
+				res.redirect('/p/' + req.params.project + '/join');
+			} else {
+				if (result[0].hangout_url) {
+					res.redirect('/p/' + req.params.project);
 				} else {
-					if (result[0].hangout_url) {
-						res.redirect('/p/' + req.params.project);
-					} else {
-						res.render('project_hangout', {
-							user: {
-								name: req.session.name,
-								username: req.session.username,
-								pid: req.session.pid
-							},
-							project: {
-								id: _project.id,
-								url: _project.url,
-								name: _project.name,
-								desc: _project.desc,
-								admin_id: _project.admin_id,
-								hangout_url: _project.hangout_url
-							},
-							csrfToken: req.csrfToken(),
-							title: _project.name
-						});
-					};
+					res.render('project_hangout', {
+						user: {
+							name: req.session.name,
+							username: req.session.username,
+							pid: req.session.pid
+						},
+						project: {
+							id: result[0].id,
+							url: result[0].url,
+							name: result[0].name,
+							desc: result[0].desc,
+							admin_id: result[0].admin_id,
+							hangout_url: result[0].hangout_url
+						},
+						csrfToken: req.csrfToken(),
+						title: result[0].name
+					});
 				};
-			});
+			};
 		});
 	};
 });
@@ -144,7 +132,6 @@ router.post('/:project/hangout', parseForm, csrfProtection, function(req, res, n
 	} else {
 		connection.query('select * from project_entries join projects on projects.id = project_entries.id where pid = ? and url = ?', [req.session.pid, req.params.project], function(err, result) {
 			if (err) throw err;
-			var _project = result[0];
 			if (result.length == 0) {
 				res.json({'status': 'fail', 'err': 'permission_denied'});
 			} else {
