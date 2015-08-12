30ebb0ece710f23bdecc779dbe5d2f0e579cf95c
Seungho Kim <niceb5y@gmail.com>
2015-08-12 16:56:45 +0900

    add hangout url saving

--- routes/project.js
+++ routes/project.js
@@ -46,7 +46,8 @@ router.get('/:project', function(req, res, next) {
 							url: _project.url,
 							name: _project.name,
 							desc: _project.desc,
-							admin_id: _project.admin_id
+							admin_id: _project.admin_id,
+							hangout_url: _project.hangout_url
 						},
 						title: _project.name
 					});
@@ -79,7 +80,8 @@ router.get('/:project/calendar', function(req, res, next) {
 							url: _project.url,
 							name: _project.name,
 							desc: _project.desc,
-							admin_id: _project.admin_id
+							admin_id: _project.admin_id,
+							hangout_url: _project.hangout_url
 						},
 						title: _project.name,
 						js: [
@@ -94,6 +96,65 @@ router.get('/:project/calendar', function(req, res, next) {
 					});
 				};
 			});
+		});
+	};
+});
+
+router.get('/:project/hangout', csrfProtection, function(req, res, next) {
+	if (!req.session.name) {
+		res.redirect('/user/login');
+	} else {
+		connection.query('select * from projects where url = ?', [req.params.project], function(err, result) {
+			if (err) throw err;
+			var _project = result[0];
+			connection.query('select * from project_entries where pid = ? and id = ?', [req.session.pid, _project.id], function(err, result) {
+				if (err) throw err;
+				if (result.length == 0) {
+					res.redirect('/p/' + req.params.project + '/join');
+				} else {
+					if (result[0].hangout_url) {
+						res.redirect('/p/' + req.params.project);
+					} else {
+						res.render('project_hangout', {
+							user: {
+								name: req.session.name,
+								username: req.session.username,
+								pid: req.session.pid
+							},
+							project: {
+								id: _project.id,
+								url: _project.url,
+								name: _project.name,
+								desc: _project.desc,
+								admin_id: _project.admin_id,
+								hangout_url: _project.hangout_url
+							},
+							csrfToken: req.csrfToken(),
+							title: _project.name
+						});
+					};
+				};
+			});
+		});
+	};
+});
+
+router.post('/:project/hangout', parseForm, csrfProtection, function(req, res, next) {
+	if (!req.session.username) {
+		res.json({'status': 'fail', 'err': 'session_expired'});
+	} else {
+		connection.query('select * from project_entries join projects on projects.id = project_entries.id where pid = ? and url = ?', [req.session.pid, req.params.project], function(err, result) {
+			if (err) throw err;
+			var _project = result[0];
+			if (result.length == 0) {
+				res.json({'status': 'fail', 'err': 'permission_denied'});
+			} else {
+				var url = req.body.url.replace('https://hangoutsapi.talkgadget.google.com/hangouts/_/', '');
+				connection.query('update projects set hangout_url = ? where id = ?', [url, result[0].id], function(err, result) {
+					if (err) throw err;
+					res.json({'status': 'success'});
+				});
+			}
 		});
 	};
 });
@@ -116,7 +177,8 @@ router.get('/:project/join', function(req, res, next) {
 					name: result[0].name,
 					desc: result[0].desc,
 					admin_id: result[0].admin_id,
-					created_date: moment(result[0].created_date).format('LL')
+					created_date: moment(result[0].created_date).format('LL'),
+					hangout_url: result[0].hangout_url
 				},
 				title: result[0].name
 			});
