60f94f6d1ef3cce2862855a216cb37a82be6bf0a
Seungho Kim <niceb5y@gmail.com>
2015-08-26 16:12:48 +0900

    bug fix

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
-	cookie: { secure: true },
+	cookie: {},
 	secret: '98bec3f956899257d0f02ae3810aa0f2',
 	resave: false,
 	saveUninitialized: true
